fc8ad4982e9329a990eabb067fdc2b6e404bea10
Darío Semino <dariomartinsemino@gmail.com>
2025-05-11 11:19:19 +0200

    add card front data

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,13 +5,29 @@ export default function Cards() {
     <div className="cards_main_container">
       <div className="card_container card_back">
         <div className="card">
-          <img className="img_card" src="/images/bg-card-back.png" alt="" />
+          <img
+            className="img_card"
+            src="/images/bg-card-back.png"
+            alt="back image of the card"
+          />
           <p className="cvc_data">000</p>
         </div>
       </div>
       <div className="card_container card_front">
         <div className="card">
-          <img className="img_card" src="/images/bg-card-front.png" alt="" />
+          <img
+            className="img_card"
+            src="/images/bg-card-front.png"
+            alt="front image of the card"
+          />
+          <img
+            className="card_logo"
+            src="/images/card-logo.svg"
+            alt="logo image of the card"
+          />
+          <p className="card_number">0000 0000 0000 0000</p>
+          <p className="card_name">JANE APPLESEED</p>
+          <p className="card_date">00/00</p>
         </div>
       </div>
     </div>
